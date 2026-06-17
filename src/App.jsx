91867a05dfa47c422ea20e6cd2a9368291377644
Yug Rai <102686454+OPwantssomePIE@users.noreply.github.com>
2026-06-17 22:38:47 +0545

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import AboutPage from './pages/About';
-import ServicesPage from './pages/Services';
+import Home from './pages/Home.jsx';
+import AboutPage from './pages/About.jsx';
+import ServicesPage from './pages/Services.jsx';
 
 export default function App() {
   return (
